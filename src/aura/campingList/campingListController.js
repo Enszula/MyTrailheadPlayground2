e3934764102bc6a0dc893920b9982a08f2991bbd
kamil.entsua-mensah <j3ac5eoP>
2019-05-22 11:47:33 +0200

    Connect to Salesforce with Server-Side Controllers

--- src/aura/campingList/campingListController.js
+++ src/aura/campingList/campingListController.js
@@ -2,24 +2,25 @@
  * Created by kamil.entsua-mensah on 21.05.2019.
  */
 ({
-    clickCreateItem : function(component, event, helper) {
-    var validCamping = component.find('campingform').reduce(function (validSoFar, inputCmp) {
-            // Displays error messages for invalid fields
-            inputCmp.showHelpMessageIfInvalid();
-            return validSoFar && inputCmp.get('v.validity').valid;
-        }, true);
+	clickCreateItem : function(component, event, helper) {
+        var isFormValid = component.find("campingItemForm").reduce(function(isValid, inputCmp){
+        	inputCmp.showHelpMessageIfInvalid();
+            return isValid && inputCmp.get("v.validity").valid;
+        });
 
-        if(validCamping){
+        if (isFormValid) {
             var newCampingItem = component.get("v.newItem");
-            //helper.createCamping(component,newCampingItem);
-            var campings = component.get("v.items");
-            var item = JSON.parse(JSON.stringify(newCampingItem));
+            helper.createItem(component,newCampingItem);
 
-            campings.push(item);
-
-            component.set("v.items",campings);
-            component.set("v.newItem",{ 'sobjectType': 'Camping_Item__c','Name': '','Quantity__c': 0,
-                                       'Price__c': 0,'Packed__c': false });
         }
+	},
+
+    doInit : function (component, event, helper) {
+        var action = component.get("c.getItems");
+        action.setCallback(this,function (response) {
+        	var campingItems = response.getReturnValue();
+            component.set("v.items",campingItems);
+        });
+        $A.enqueueAction(action);
     }
 })
